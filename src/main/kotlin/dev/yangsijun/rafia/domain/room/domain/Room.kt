1e96379e18c4f4838edfed89cf175c787349fc98
YangSijun528 <yangsijun5528@gmail.com>
2023-06-23 00:10:36 +0900

    게임 관련 객체 추가

--- src/main/kotlin/dev/yangsijun/rafia/domain/room/domain/Room.kt
+++ src/main/kotlin/dev/yangsijun/rafia/domain/room/domain/Room.kt
@@ -1,5 +1,6 @@
 package dev.yangsijun.rafia.domain.room.domain
 
+import dev.yangsijun.rafia.data.player.Player
 import dev.yangsijun.rafia.domain.room.enums.RoomStatus
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
@@ -11,11 +12,12 @@ class Room(
     @Id
     val id: String = UUID.randomUUID().toString(),
     val name: String,
-    val status: RoomStatus
+    val status: RoomStatus,
+    val players: MutableList<Player>
     // TODO 그외 값 - 플레이어 수, 게임(게임 객체, 게임 시작 시 생성) 등
     //  그리고 게임 시작하고 나서 게임 정보 저장할때는 비동기로 해야함
     //  가져와서 정보수정 -> save() -> 리턴 -> 전달
     //                     ㄴ-> db에 저장
 ) {
-    constructor() : this("", "", RoomStatus.OPEN)
+    constructor() : this("", "", RoomStatus.OPEN, mutableListOf() )
 }
