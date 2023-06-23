b3ae15eb00363e8cb7af0557393808d35e3b9d24
YangSijun528 <yangsijun5528@gmail.com>
2023-06-23 09:44:35 +0900

    fix constructor default value

--- src/main/kotlin/dev/yangsijun/rafia/domain/room/domain/Room.kt
+++ src/main/kotlin/dev/yangsijun/rafia/domain/room/domain/Room.kt
@@ -12,8 +12,8 @@ class Room(
     @Id
     val id: String = UUID.randomUUID().toString(),
     val name: String,
-    val status: RoomStatus,
-    val players: MutableList<Player>
+    val status: RoomStatus = RoomStatus.OPEN,
+    val players: MutableList<Player> = mutableListOf()
     // TODO 그외 값 - 플레이어 수, 게임(게임 객체, 게임 시작 시 생성) 등
     //  그리고 게임 시작하고 나서 게임 정보 저장할때는 비동기로 해야함
     //  가져와서 정보수정 -> save() -> 리턴 -> 전달
